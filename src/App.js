40b5b213cbe7c0cd0ece3b99bf065734c9bcd2c4
SophieL-64 <sophie.lamant31@gmail.com>
2022-07-12 22:28:55 +0200

    version finalisée

--- src/App.js
+++ src/App.js
@@ -1,6 +1,42 @@
+import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import axios from "axios";
+import Home from "./screens/Home";
+
 function App() {
+  const [caverns, setCaverns] = useState([]);
+  const [cavernsFiltered, setCavernsFiltered] = useState([]);
+
+  useEffect(() => {
+    const getCaverns = () => {
+      axios
+        .get("https://sophiel-64.github.io/DTG-dans-ta-grotte-API/api/all.json")
+        .then((response) => {
+          setCaverns(response.data);
+          setCavernsFiltered(response.data);
+        });
+    };
+    getCaverns();
+  }, []);
+
   return (
     <div className="App">
+      {console.log(cavernsFiltered) ||
+        (cavernsFiltered &&
+          cavernsFiltered.map((cavern) => <h1>{cavern.name}</h1>))}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Home
+              caverns={caverns}
+              setCaverns={setCaverns}
+              cavernsFiltered={cavernsFiltered}
+              setCavernsFiltered={setCavernsFiltered}
+            />
+          }
+        />
+      </Routes>
     </div>
   );
 }
