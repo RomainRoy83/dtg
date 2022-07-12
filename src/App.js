57fa2ccd0c899bb1e35a381a48d3b914863ae4cb
LAURE CARILLO <laure.carillo@gmail.com>
2022-07-12 22:52:43 +0200

    Premier push - Screen ok - css a faire

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import CavernDetails from "./screens/CavernDetails";
+import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function App() {
+  const [caverns, setCaverns] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://sophiel-64.github.io/DTG-dans-ta-grotte-API/api/all.json")
+      .then((res) => setCaverns(res.data));
+  }, []);
   return (
     <div className="App">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <CavernDetails
+              caverns={caverns}
+              setCaverns={setCaverns}
+            />
+          }
+        />
+      </Routes>
     </div>
   );
 }
